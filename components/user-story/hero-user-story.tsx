6d38c54400f220a3e27e45a61b2983836dd415b1
Rifki Andriyanto <andriyantorifki@gmail.com>
2025-11-27 10:52:57 +0700

    feat: add content

--- components/user-story/hero-user-story.tsx
+++ components/user-story/hero-user-story.tsx
@@ -9,19 +9,10 @@ export default function HeroUserStory() {
         <div className="grid items-center gap-8 py-12 md:grid-cols-2 md:py-20">
           <div className="text-center md:text-left order-last md:order-first">
             <h1 className="mb-4 text-4xl font-extrabold leading-tight tracking-tighter text-white md:text-5xl">
-              <span className="text-custom-yellow">Fast Digital Credit</span>{" "}
-              for Filipinos
+              <span className="text-custom-yellow">
+                Meet Our Happy Borrowers{" "}
+              </span>
             </h1>
-            <div className="mx-auto max-w-lg md:mx-0">
-              <p className="mb-8 text-lg text-gray-300">
-                Happycash is a fintech platform that delivers digital credit to
-                Filipinos - providing quick, seamless, affordable financial
-                access to the credit-worthy yet underserved. Happycash is
-                operated by Yinshan Lending Inc., a licensed lending company
-                with SEC Registration No. CS201900108 and Certificate of
-                Authority No. 2860.
-              </p>
-            </div>
           </div>
 
           <div className="flex items-center justify-center order-first md:order-last">
