7eb27cfb7b0efd8c9436e40575046a17580aa311
Rifki Andriyanto <andriyantorifki@gmail.com>
2025-12-03 14:54:36 +0700

    fix: update hero

--- app/(default)/get-app/page.tsx
+++ app/(default)/get-app/page.tsx
@@ -1,12 +1,12 @@
 import GetApp from "@/components/get-app/get-app";
-import HeroSecurity from "@/components/security/hero-security";
+// import HeroSecurity from "@/components/security/hero-security";
 import { getMetadata } from "@/lib/site-metadata"; // Import Helper
 
 export const metadata = getMetadata("getApp");
 export default function Home() {
   return (
     <>
-      <HeroSecurity />
+      {/* <HeroSecurity /> */}
       <GetApp />
     </>
   );
